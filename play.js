c1e3528b901388a08d649958ba5e19dc2f5a1eb0
DemouraGus <demoura.gus@gmail.com>
2024-11-20 08:34:00 -0800

    stdin function created and tested with exit command

--- play.js
+++ play.js
@@ -1,5 +1,20 @@
 const connect = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
-console.log("Connecting ...");
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
 connect();
+setupInput();
+
